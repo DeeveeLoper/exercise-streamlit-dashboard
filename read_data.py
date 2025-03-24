9544d2e4d10f4bccad75fbccac54459a1cee9b1e
Roberto C. <roberto.camacho4@gmail.com>
2025-03-24 11:55:08 +0100

    Corrected the code in file read_data

--- read_data.py
+++ read_data.py
@@ -5,10 +5,7 @@ def read_data():
     
     data_path = Path(__file__).parent
     df = pd.read_csv(data_path / "data" / "supahcoolsoft.csv", skiprows=5)
-    
-    df['Age'] = pd.to_numeric(df['Age'], errors='coerce')
-    df['Salary_SEK'] = pd.to_numeric(df['Salary_SEK'], errors='coerce')
-    
+        
     return df
 
 if __name__ == '__main__':
